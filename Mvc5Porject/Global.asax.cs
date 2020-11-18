323df866400908f90918f26ec689644c906aeb7a
克雷斯 <hgkiller@hotmail.com>
2020-11-18 09:51:13 +0800

    加入DI容器

--- Mvc5Porject/Global.asax.cs
+++ Mvc5Porject/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,45 @@ namespace Mvc5Porject
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var services = new ServiceCollection();
+            ConfigureService(services);
+
+            var resolver = new DotnetCoreDIDependencyResolver(services.BuildServiceProvider());
+        }
+
+        private void ConfigureService(ServiceCollection services)
+        {
+            var controllers = typeof(MvcApplication).Assembly.GetExportedTypes()
+                .Where(x => !x.IsAbstract)
+                .Where(x => typeof(IController).IsAssignableFrom(x))
+                .Where(x => x.Name.EndsWith("Controller"));
+            foreach(var ctrl in controllers)
+            {
+                services.AddTransient(ctrl);
+            }
+
+            services.AddHttpClient();
+        }
+    }
+
+    internal class DotnetCoreDIDependencyResolver : IDependencyResolver
+    {
+        private IServiceProvider serviceProvider;
+
+        public DotnetCoreDIDependencyResolver(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.serviceProvider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.serviceProvider.GetServices(serviceType);
         }
     }
 }
